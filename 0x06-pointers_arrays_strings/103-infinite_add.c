785ca13e08cc00327f1dbb6b703a8807a031a1dd
rmatovu987 <rmatovu987@gmail.com>
2021-07-01 02:11:05 -0700

    error

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,33 +1,66 @@
-#include "holberton.h"
-
 /**
- * infinite_add - function with 4 arguments
- * @n1: char type pointer argument
- * @n2: char type pointer argument
- * @r: char type pointer argument
- * @size_r: int type argument
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
  *
- * Description: adds two numbers from string
- * Return: sum of two integers
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int count, count2;
+int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+char tmp[10000];
 
-while (n1[count] != '\0')
-count++;
-while (n2[count2] != '\0')
-count2++;
-
-*r = *(r + size_r);
-while (n1[count] > 0 || n1[count2] > 0)
+rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+while (n1[l1] != '\0')
+l1++;
+while (n2[l2] != '\0')
+l2++;
+if (l1 + 2 > size_r || l2 + 2 > size_r)
+return (0);
+l1--;
+l2--;
+while (i <= l1 || i <= l2)
 {
-if (n1[count] + n2[count2] > 0)
-*r = n1[count - 1] + n2[count2 - 1] + 1;
+num1 = num2 = 0;
+if (i <= l1)
+num1 = n1[l1 - i] - '0';
+if (i <= l2 && (l2 - i) >= 0)
+num2 = n2[l2 - i] - '0';
+sum = num1 + num2 + carry;
+if (sum >= 10)
+{
+carry = 1;
+sum -= 10;
+}
 else
-*r = n1[count] + n2[count2];
-count--;
-count2--;
+carry = 0;
+r[i] = sum + '0';
+i++;
+rl++;
+}
+if (carry > 0)
+{
+r[i] = carry + '0';
+r[i + 1] = '\0';
+}
+i = tmpl = 0;
+while (i <= rl)
+{
+tmp[i] = r[rl - i];
+tmpl++;
+i++;
+}
+i = 0;
+while (i < tmpl)
+{
+if (r[i] == '\0')
+{
+break;
+}
+r[i] = tmp[i];
+i++;
 }
 return (r);
 }
