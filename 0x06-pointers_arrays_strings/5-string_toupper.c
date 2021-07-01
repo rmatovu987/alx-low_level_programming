31961e6596b6f29e1372f854fc54a4007f68a431
rmatovu987 <rmatovu987@gmail.com>
2021-06-30 23:59:30 -0700

    Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *string_toupper - convert lowercase to uppercase
+ *@t : char
+ *Return: char *s upper
+ */
+char *string_toupper(char *t)
+{
+char *s = t;
+char check;
+
+while (*s)
+{
+for (check = 'a'; check <= 'z'; check++)
+{
+if (*s == check)
+*s = *s - 32;
+}
+s++;
+}
+
+return (t);
+
+}
