fca552320bd8c8c6ca7239fca42322ba06b4597a
rmatovu987 <rmatovu987@gmail.com>
2021-07-01 00:51:41 -0700

    Encoder

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_number(3);
+_putchar('\n');
+print_number(-1);
+_putchar('\n');
+print_number(10);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+print_number(-99);
+_putchar('\n');
+print_number(2147483647);
+_putchar('\n');
+print_number(-2147483648);
+_putchar('\n');
+
+
+return (0);
+}
