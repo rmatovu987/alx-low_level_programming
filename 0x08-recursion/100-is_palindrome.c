f2eea5a173398fba463f80146bc31058bf24d853
rmatovu987 <rmatovu987@gmail.com>
2021-07-07 01:13:07 -0700

    Error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,58 +1,47 @@
 #include "holberton.h"
 
-int is_palindrome(char *s);
-int palcheck(char *s, int x, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- *_strlen-length of string
- *@s:char
- *Return:int
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
  */
-
 int is_palindrome(char *s)
 {
-int length;
-int i = 0;
-
-length = _strlen(s);
-if (length > 1)
-return (palcheck(s, length - 1, i));
-
+if (*s == 0)
 return (1);
-
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- *_strlen-length of string
- *@s:char
- *Return:int
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
  */
-
-int _strlen(char *s)
+int _strlen_recursion(char *s)
 {
-if (*s)
-return (1 + _strlen(s + 1));
+if (*s == '\0')
 return (0);
+return (1 + _strlen_recursion(s + 1));
 }
-
 
 /**
- *palcheck - checks palindrome
- *@s:char
- *@x:int
- *@i:int
- *Return:1
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
  */
-
-int palcheck(char *s, int x, int i)
+int check_pal(char *s, int i, int len)
 {
-
-if (i <= (x / 2) && s[i] == s[x])
-{
-return (palcheck(s, x - 1, i + 1));
-}
-else if (i > (x / 2))
-return (1);
-else
+if (*(s + i) != *(s + len - 1))
 return (0);
+if (i >= len)
+return (1);
+return (check_pal(s, i + 1, len - 1));
 }
