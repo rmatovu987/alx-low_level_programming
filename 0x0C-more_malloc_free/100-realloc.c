07b1363ebf108193b63c3486fcc3e9cd4bb97e78
rmatovu987 <rmatovu987@gmail.com>
2021-07-16 19:19:19 -0400

    ..

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,59 +1,50 @@
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * _memcpy - Copy n bytes from memory area src to memory area dest
- * @dest: Memory area to copy to
- * @src: Memory area to copy from
- * @n: Amount to copy from memory area
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
  *
- * Return: Pointer to area
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
-}
-
-/**
- * _realloc - Reallocate a memory block using malloc
- * @ptr: Old memory block
- * @old_size: Size of of old memory block
- * @new_size: Size the new memory block should be
- *
- * Return: Pointer to new memory space, NULL if it fails
+ * Return: pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *nptr;
-	unsigned int min;
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
 
-	if (ptr == NULL)
-	{
-		nptr = malloc(new_size);
-		return (nptr);
-	}
-	if (ptr != NULL && new_size == 0)
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size < old_size)
-		min = new_size;
-	else
-		min = old_size;
-	nptr = malloc(new_size);
-	if (nptr == NULL)
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
 		return (NULL);
-	nptr = _memcpy(nptr, ptr, min);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
 	free(ptr);
-	return (nptr);
+	return (ptr1);
 }
