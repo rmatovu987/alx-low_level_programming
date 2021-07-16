ecd2d440a6e9a77d940a8d5838a951aabc623209
rmatovu987 <rmatovu987@gmail.com>
2021-07-16 09:02:08 -0400

    ..

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,8 +16,8 @@ unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 for (len1 = 0; s1[len1] != '\0'; len1++)
 ;
-for (len2 = 0; s2[len2] != '\0'; len2++)
-;
+while (s2 && s2[len2])
+len2++;
 
 s = (char *)malloc((len1 + n + 1) * sizeof(char));
 
