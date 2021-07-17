de86b7599f7e8ddb7e673a587dfcd2b9af0e8ff4
rmatovu987 <rmatovu987@gmail.com>
2021-07-16 20:23:06 -0400

    ..

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,6 +35,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		max = old_size;
 	for (i = 0; i < max; i++)
 		p[i] = oldp[i];
+
+		p[i] = '\0';
 	free(ptr);
 	return (p);
 }
