c3bdd9a14f355a85e4f9c51a2849bdef3eb0d200
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 17:27:03 -0400

    ...

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,6 +9,8 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+int i;
+
 op_t ops[] = {
 {"+", op_add},
 {"-", op_sub},
@@ -17,12 +19,12 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
-int i;
+
 i = 0;
 
-while (ops[i].op)
+while (i < 5)
 {
-if (*s == *ops[i].op)
+if ((*s == *ops[i].op) && !(*(s + 1))
 return (ops[i].f);
 
 i++;
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,22 +7,25 @@
  */
 int main(int argc, char *argv[])
 {
-int val;
+int num1, num2;
 
 if (argc != 4)
 printf("Error\n");
 exit(98);
 
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
 if (get_op_func(argv[2]) == NULL)
 printf("Error\n");
 exit(99);
 
-if ((strcmp(argv[2], "/") == 0 || (strcmp(argv[2], "%") == 0)) &&
-atoi(argv[3]) == 0)
-printf("Error\n");
+if ((*argv[2] == '%' || *argv[2] == '\\') && num2 == 0)
+{
+printf("Error'\n");
 exit(100);
+}
 
-val = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
-printf("%d\n", val);
+printf("%d\n", (get_op_func(argv[2])(num1, num2)));
 return (0);
 }
