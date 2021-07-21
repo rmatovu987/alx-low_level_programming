d34e4fdf3aab133b9de7fc3e662bce4771782365
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 16:46:53 -0400

    ...

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,29 +7,25 @@
  */
 int main(int argc, char *argv[])
 {
-  int val;
+int val;
+int (*func)(int, int);
 
-  if (argc != 4)
-  {
-    printf("Error\n");
-    exit(98);
-  }
+if (argc != 4)
+printf("Error\n");
+exit(98);
 
-  func = get_op_func(argv[2]);
+func = get_op_func(argv[2]);
 
-  if (func == NULL)
-  {
-    printf("Error\n");
-    exit(99);
-  }
+if (func == NULL)
+printf("Error\n");
+exit(99);
 
-  if ((strcmp(argv[2], "/") == 0 || (strcmp(argv[2], "%") == 0)) &&
-      atoi(argv[3]) == 0)
-      {
-    printf("Error\n");
-    exit(100);
-  }
-  val = func(atoi(argv[1]), atoi(argv[3]));
-  printf("%d\n", val);
-  return (0);
+if ((strcmp(argv[2], "/") == 0 || (strcmp(argv[2], "%") == 0)) &&
+atoi(argv[3]) == 0)
+printf("Error\n");
+exit(100);
+
+val = func(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", val);
+return (0);
 }
