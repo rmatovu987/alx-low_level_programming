b5e0b5995c59f2222187a439ee525b34732b25d5
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 18:19:58 -0400

    ..

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,24 +8,24 @@
   */
 int (*get_op_func(char *s))(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
 
-	i = 0;
-	while (i <= 5)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	return (NULL);
+i = 0;
+while (i <= 5)
+{
+if (strcmp(ops[i].op, s) == 0)
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
 }
