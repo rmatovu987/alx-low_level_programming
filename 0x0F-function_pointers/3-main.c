d3bf18deb3bc09b7e519511fac7b4cc098e54797
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 13:39:09 -0400

    ...

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,7 @@ printf("Error\n");
 exit(98);
 }
 if ((strcmp(argv[2], "+") == -1) || (strcmp(argv[2], "-") == -1) ||
-(strcmp(argv[2], "*") == -1) || (strcmp(argv[2], "/") == -1) || 
+(strcmp(argv[2], "*") == -1) || (strcmp(argv[2], "/") == -1) ||
 (strcmp(argv[2], "%") == -1))
 {
 printf("Error\n");
