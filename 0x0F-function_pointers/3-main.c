0a8cec9e999dfe43f16f3321cedec5234c3d5423
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 13:44:54 -0400

    ...

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,20 +11,20 @@ int val;
 
 if (argc - 1 > 3 || argc - 1 < 3)
 {
-printf("Error\n");
+printf("Error98\n");
 exit(98);
 }
 if ((strcmp(argv[2], "+") == -1) || (strcmp(argv[2], "-") == -1) ||
 (strcmp(argv[2], "*") == -1) || (strcmp(argv[2], "/") == -1) ||
 (strcmp(argv[2], "%") == -1))
 {
-printf("Error\n");
+printf("Error99\n");
 exit(99);
 }
 if ((strcmp(argv[2], "/") == 0 || (strcmp(argv[2], "%") == 0)) &&
 atoi(argv[3]) == 0)
 {
-printf("Error\n");
+printf("Error100\n");
 exit(100);
 }
 val = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
