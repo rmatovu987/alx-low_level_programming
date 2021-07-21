12bdf07b4184bfde4db1b2a4e6086b998dec592c
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 17:50:02 -0400

    ..

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,56 +1,62 @@
 #include "3-calc.h"
 
 /**
- *op_add-function that adds
- *@a: integer
- *@b: integer
- *Return: int
- */
+  * op_add - function to add two integers
+  * @a: first integer value
+  * @b: second integer value
+  *
+  * Return: sum of the two integers
+  */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
- *op_sub-function that subtracts
- *@a: integer
- *@b: integer
- *Return: int
- */
+  * op_sub - function to subtract
+  * @a: first integer value
+  * @b: second intger value
+  *
+  * Return: subtacted value
+  */
+
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
- *op_mul-function that multiplies
- *@a: integer
- *@b: integer
- *Return: int
- */
+  * op_mul - function to multiply two integers
+  * @a: first integer value
+  * @b: second integer value
+  *
+  * Return: multiplied value
+  */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
 /**
- *op_div-function that divides
- *@a: integer
- *@b: integer
- *Return: int
- */
+  * op_div - function to divide numbers
+  * @a: first integer value
+  * @b: second integer value
+  *
+  * Return: multiplied value
+  */
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
 
 /**
- *op_mod-function that adds
- *@a: integer
- *@b: integer
- *Return: int
- */
+  * op_mod - function to find the remainder
+  * @a: first integer value
+  * @b: second integer value
+  *
+  * Return: remainder value
+  */
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
