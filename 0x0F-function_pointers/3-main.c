c54a63615b0b89aa0cf522734a5e41bc68baddca
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 13:23:53 -0400

    ...

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  *main - main function
  *@argc:int
@@ -19,11 +16,11 @@ exit(98);
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 
-if (get_op_func(argv[2]) == NULL)
+if (!get_op_func(argv[2]))
 printf("Error\n");
 exit(99);
 
-if ((*argv[2] == '%' || *argv[2] == '\\') && num2 == 0)
+if ((*argv[2] == '%' || *argv[2] == '/') && num2 == 0)
 printf("Error'\n");
 exit(100);
 
