74606d35d4760dbf58aa8861c256ae2ea0ba531e
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 13:31:51 -0400

    ...

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,23 +7,26 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2;
+int val;
 
-if (argc != 4)
+if (argc - 1 > 3 || argc - 1 < 3)
+{
 printf("Error\n");
 exit(98);
-
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-
-if (!get_op_func(argv[2]))
+}
+if ((strcmp(argv[2], "+") == -1) || (strcmp(argv[2], "-") == -1) ||
+(strcmp(argv[2], "*") == -1) || (strcmp(argv[2], "/") == -1) || (strcmp(argv[2], "%") == -1))
+{
 printf("Error\n");
 exit(99);
-
-if ((*argv[2] == '%' || *argv[2] == '/') && num2 == 0)
-printf("Error'\n");
+}
+if ((strcmp(argv[2], "/") == 0 || (strcmp(argv[2], "%") == 0)) &&
+atoi(argv[3]) == 0)
+{
+printf("Error\n");
 exit(100);
-
-printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+}
+val = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", val);
 return (0);
 }
