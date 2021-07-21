35285fbcca3abf450f1df64f878fd1e95247de92
rmatovu987 <rmatovu987@gmail.com>
2021-07-21 17:27:38 -0400

    ...

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ if (get_op_func(argv[2]) == NULL)
 printf("Error\n");
 exit(99);
 
-if ((*argv[2] == '%' || *argv[2] == '\\') && num2 == 0)
+if ((*argv[2] == '%' || *argv[2] == '/') && num2 == 0)
 {
 printf("Error'\n");
 exit(100);
