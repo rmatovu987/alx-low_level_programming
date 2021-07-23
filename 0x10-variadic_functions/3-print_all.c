3b0bb4e6bfa1130269733aebbf552dcc5914954b
rmatovu987 <rmatovu987@gmail.com>
2021-07-22 21:47:01 -0400

    ..

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,43 +6,44 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str, *sep = "";
+va_list ap;
+unsigned int i = 0, j, k = 0;
+char *str;
+const char t_arg[] = "cifs";
 
-	va_list list;
-
-	va_start(list, format);
-
-	if (format)
-	{
-		while (format[i])
-		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", sep, va_arg(list, int));
-					break;
-				case 'i':
-					printf("%s%d", sep, va_arg(list, int));
-					break;
-				case 'f':
-					printf("%s%f", sep, va_arg(list, double));
-					break;
-				case 's':
-					str = va_arg(list, char *);
-					if (!str)
-					printf("nill");
-					printf("%s%s", sep, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			sep = ", ";
-			i++;
-		}
-	}
-
-	printf("\n");
-	va_end(list);
+va_start(ap, format);
+while (format && format[i])
+{
+j = 0;
+while (t_arg[j])
+{
+if (format[i] == t_arg[j] && k)
+{
+printf(", ");
+break;
+} j++;
+}
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(ap, int)), k = 1;
+break;
+case 'i':
+printf("%d", va_arg(ap, int)), k = 1;
+break;
+case 'f':
+printf("%f", va_arg(ap, double)), k = 1;
+break;
+case 's':
+str = va_arg(ap, char *), k = 1;
+if (!str)
+{
+printf("(nil)");
+break;
+}
+printf("%s", str);
+break;
+} i++;
+}
+printf("\n"), va_end(ap);
 }
