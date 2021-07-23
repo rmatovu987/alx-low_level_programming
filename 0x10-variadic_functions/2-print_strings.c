248759811ac412e8a82d8303eef894f7c60a04b5
rmatovu987 <rmatovu987@gmail.com>
2021-07-22 20:46:36 -0400

    ..

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+*print_strings - function that prints the numbers
+*@separator: string that separates the numbers
+*@n: number of integers to be printed
+*Return: void
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+char *s;
+va_list ap;
+
+va_start(ap, n);
+for (i = 0; i < n; i++)
+s = va_arg(ap, char *);
+if (!s)
+s = "(nil)";
+if (!separator || (separator && i == 0))
+printf("%s", s);
+else
+printf("%s%s", separator, s);
+
+va_end(ap);
+printf("\n");
+}
