c44ed6d83384ae830af7d125f8448748d17dc2a7
rmatovu987 <rmatovu987@gmail.com>
2021-08-03 13:33:42 -0400

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp->next = ptr;
 			return (ptr);
 		} else
-			temp = temp->next
+			temp = temp->next;
 	}
 	return (NULL);
 }
