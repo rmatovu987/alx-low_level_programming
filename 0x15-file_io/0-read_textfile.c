cf86631c92e47f608bada62ae02207e7854a678c
rmatovu987 <rmatovu987@gmail.com>
2021-08-09 17:18:24 +0300

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,52 @@
 #include "main.h"
-
 /**
- *read_textfile - function to read a text file
- *@filename: name of file to read
- *@letters: number of letters it should read and print
- *Return: actual number of letters it read and printed
+ * read_textfile - reads a file and prints it
+ * @filename: const char*
+ * @letters: size_t
+ * Return: ssize_t
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, s, t;
+	int fd, r, w, c;
 	char *buf;
+
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDWR);
+	buf = malloc(sizeof(char) * letters + 1);
 
-	if (fd < 0)
+	if (!buf)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 	{
-		buf = malloc(sizeof(char) * letters);
-		if (buf == NULL)
-			return (0);
-
-		s = read(fd, buf, letters);
-		if (s < 0)
-		{
-			free(buf);
-			return (0);
-		}
-		buf[s] = '\0';
-
-		t = write(STDOUT_FILENO, buf, s);
-		if (t < 0)
-		{
-			free(buf);
-			return (0);
-		}
-
 		free(buf);
-		close(fd);
-		return (t);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		return (0);
 	}
 
-	return (0);
+	w = write(STDOUT_FILENO, buf, r);
+
+	if (w == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	c = close(fd);
+	if (c == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (w);
 }
+
