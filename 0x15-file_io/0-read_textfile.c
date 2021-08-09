8b53c456e19e96af6963a4632674fdb8f7b60a86
rmatovu987 <rmatovu987@gmail.com>
2021-08-09 14:40:40 +0300

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ *read_textfile - function to read a text file
+ *@filename: name of file to read
+ *@letters: number of letters it should read and print
+ *Return: actual number of letters it read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, s, t;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+
+	if (fd < 0)
+	{
+		buf = malloc(sizeof(char) * letters);
+		if (!buf)
+			return (0);
+
+		s = read(fd, buf, letters);
+		if (s < 0)
+		{
+			free(buf);
+			return (0);
+		}
+		buf[s] = '\0';
+
+		close(fd);
+
+		t = write(STDOUT_FILENO, buf, s);
+		if (t < 0)
+		{
+			free(buf);
+			return (0);
+		}
+
+		free(buf);
+		return (t);
+	}
+
+	return (0);
+}
