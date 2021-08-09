3cf053c245ba218e177ece860848354657aae1fc
rmatovu987 <rmatovu987@gmail.com>
2021-08-09 17:27:47 +0300

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,14 +10,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len, x;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if(!text_content)
+	if (!text_content)
 		return (1);
 
 	for (len = 0; text_content[len] != '\0'; len++)
