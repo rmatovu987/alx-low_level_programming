6f7c4d1dd219fc2c4c3d8faf0489dd01de551b06
rmatovu987 <rmatovu987@gmail.com>
2021-09-16 20:46:48 +0300

    Task 1

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,53 +1,39 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete node
- * @head: dlistint_t **
- * @index: int
- * Return: int
+ * delete_dnodeint_at_index -   deletes the node at index index of
+ * a dlistint_t linked list.
+ * @head: pointer to head of the list
+ * @index: index of node to delete
+ *
+ * Return: 1 if it succeeded, -1 if it failed
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int i = 0;
+	dlistint_t *cur = *head;
 
-	dlistint_t *temp = *head;
-	dlistint_t *hold = temp;
-	unsigned int counter =  0;
-
-	if (!head || !(*head))
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		if (!(*head)->next)
-		{
-			free(temp);
-			*head = NULL;
-			return (1);
-		}
-		else
-		{
-			temp->next->prev = NULL;
-			(*head) = (*head)->next;
-			free(temp);
-			return (1);
-		}
+		(*head) = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(cur);
+		return (1);
 	}
-	while (temp)
+	while (i < index)
 	{
-		if (counter + 1 == index)
-		{
-			hold = hold->next;
-			if (!temp->next->next)
-				temp->next = NULL;
-			else
-			{
-				temp->next = temp->next->next;
-				temp->next->prev = temp;
-			}
-			free(hold);
-			return (1);
-		}
-		temp = temp->next;
-		hold = hold->next;
-		counter++; }
-	return (-1); }
+		if (cur == NULL)
+			return (-1);
+		cur = cur->next;
+		++i;
+	}
+	cur->prev->next = cur->next;
+	if (cur->next != NULL)
+		cur->next->prev = cur->prev;
+	free(cur);
+	return (1);
+}
