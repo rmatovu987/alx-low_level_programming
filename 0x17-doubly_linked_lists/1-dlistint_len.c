bfdccdc57d61086b967ba6b99049acf334d30bc2
rmatovu987 <rmatovu987@gmail.com>
2021-09-16 20:34:09 +0300

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,12 +7,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-  size_t count = 0;
+	size_t count = 0;
 
-  while (h)
-    {
-      count++;
-      h = h->next;
-    }
-  return (count);
+	while (h)
+		{
+			count++;
+			h = h->next;
+		}
+	return (count);
 }
