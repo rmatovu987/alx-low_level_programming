5561f2b28f55aa6fbdc67865453960bed96c72fa
rmatovu987 <rmatovu987@gmail.com>
2021-09-16 10:30:54 -0700

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - number of elements in list
+ * @h: const dlistint_t
+ * Return: len
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
   size_t count = 0;
