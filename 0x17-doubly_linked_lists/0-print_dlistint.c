8158c4c9e24bae735b09f7483345411e6b3928e7
rmatovu987 <rmatovu987@gmail.com>
2021-09-16 20:08:11 +0300

    Task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elements in double linked list
+ * @h: dlistint_t const
+ * Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
